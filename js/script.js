5dd7112caef9459e65ba56d4546f0e1562990d55
lucagad <lucagaddini@icloud.com>
2022-05-03 14:20:02 +0200

    {JS} Added: Initialization Vue app and dynamics elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+// nome repo: vue-slider
+
+// Descrizione:
+// Partendo dal markup consegnate rendere dinamici tutti i contenuti (foto e testi) e al click su una thumb, visualizzare in grande l’immagine corrispondente
+
+// Bonus:
+// 1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+
+
+
 const slides = [
     {
         image: 'img/01.jpg',
@@ -27,3 +39,51 @@ const slides = [
 ];
 
 console.log(slides);
+
+const app = new Vue ({
+
+    el: '#app',
+    data:{
+
+        index: 0,
+        images :[
+            {
+                imgUrl: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+
+            {
+                imgUrl: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+
+            {
+                imgUrl: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+
+            {
+                imgUrl: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+
+            {
+                imgUrl: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti autveniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+        ],
+
+
+    },
+
+    methods: {
+
+
+    }
+
+});
